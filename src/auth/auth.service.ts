0c42e0e315261a93cde5ec8f8e55a39a0acb7c14
vitorugo1 <hvitor79@gmail.com>
2025-10-08 20:16:08 -0300

    Body - criando e recebendo o corpo da requisição POST, PATCH e PUT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'Olá do AuthService';
+  login(loginDto: LoginDto) {
+    return loginDto;
   }
 }
